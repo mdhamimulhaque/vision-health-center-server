d2eee64481e2355e4d485527d08abf4aedcb350b
Md Hamimul Haque <mdhamimulhaque@gmail.com>
2022-11-18 20:52:36 +0600

    jwt token with dashboard

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 
@@ -9,29 +10,46 @@ const port = process.env.PORT || 5000;
 
 
 
-// ---> middle ware
+// -----> middle ware
 app.use(cors());
 app.use(express.json());
 
-// ---> test
+// -----> test
 app.get('/', (req, res) => {
     res.send('vision health server is running...')
 })
 
-// mongodb connection
+//-----> mongodb connection
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.76zc9vk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// ---> working function
-async function run() {
+// ---> custom middle ware for jwt
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+    const token = authHeader.split(' ')[1];
 
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+// -----> working function
+async function run() {
     try {
-        // ---> collections
+        // -----> collections
         const appointmentServicesCollection = client.db("visionHealthCenter").collection('appointmentServices');
         const bookingsCollection = client.db("visionHealthCenter").collection('bookings');
+        const usersCollection = client.db("visionHealthCenter").collection('users');
 
-        //---> get services data
+        //-----> get services data
         app.get('/appointmentServices', async (req, res) => {
             const date = req.query.date;
             const query = {};
@@ -46,16 +64,31 @@ async function run() {
                 const appServiceBooked = alreadyBooked.filter(book => book.treatment === appointService.serviceTitle);
                 // -->all slots
                 const bookedSlots = appServiceBooked.map(book => book.slot);
-                // ---> remaining slots
+                // --> remaining slots
                 const remainingSlots = appointService.slots.filter(slot => !bookedSlots.includes(slot));
                 appointService.slots = remainingSlots;
             })
-
-
             res.send(appointmentServices)
         })
 
-        // ---> booking || post
+        // -----> booking with email query
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+
+            const query = { email: email };
+            const booking = await bookingsCollection.find(query).toArray();
+            res.send(booking)
+        })
+
+
+
+        // -----> booking || post
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const query = {
@@ -73,7 +106,28 @@ async function run() {
 
             const result = await bookingsCollection.insertOne(booking);
             res.send(result)
+        });
+
+        // ---> jwt 
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: '' })
         })
+
+        // ---> users data store || Post
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const users = await usersCollection.insertOne(user);
+            res.send(users)
+        })
+
+
 
     } finally { }
 
